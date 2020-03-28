8ea259bfcf4d63c6b4a3c833b578e0d3dc84acd5
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-03-28 14:32:42 -0500

    fixed lodash imports

---
+++ src/App-works.svelte
@@ -0,0 +1,12 @@
+<script>
+  import _ from 'lodash';
+
+  let name = 'Svelvet';
+</script>
+
+<h1>Hello, {_.startCase(name)}!</h1>
+
+<label>
+  Name
+  <input bind:value="{name}" />
+</label>
--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,31 @@
-<script>
-  import _ from '/web_modules/lodash';
-  import DateDisplay from './DateDisplay';
-  import Foo from './Foo.js';
+<script lang="typescript">
+  import _ from 'lodash';
+  //import DateDisplay from './DateDisplay';
+  //import Foo from './Foo.js';
 
   import dogs from './dogs.json';
   console.log('dogs =', dogs);
 
-  let name = 'Svelvet';
+  let name: string = 'Svelvet';
+
+  function add(n1: number, n2: number): number {
+    return n1 + n2;
+  }
+
+  const sum = add(2, '3');
 </script>
 
 <h1>Hello, {_.startCase(name)}!</h1>
 
 <label>
   Name
-  <input bind:value={name}>
+  <input bind:value="{name}" />
 </label>
 
-<DateDisplay />
-<Foo />
+<!--DateDisplay />
+<Foo /-->
+
+<div>sum = {sum}</div>
 
 <style>
   h1 {
